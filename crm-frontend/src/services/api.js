5f59c691cd2019b764f7ecb66a064392417c0b1e
Migaazz <miguelgh.ms@gmail.com>
2026-04-05 23:08:36 -0300

    chore: configuração para produção

--- crm-frontend/src/services/api.js
+++ crm-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:8080'
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8080'
 });
 
 // Este "interceptor" roda antes de cada requisição que o axios faz
